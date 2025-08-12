f50a174d33960ea307ae9ffce4cfe937d1bf71e1
Ruchi-coder2004 <ruchithashivaswamy03@gmail.com>
2025-08-12 09:40:59 +0530

    added p1 and p2 python files

---
+++ day1/p2.py
@@ -0,0 +1,5 @@
+#Check if the number is even
+input_number = int(input('Enter a number to check if it is Even: '))
+
+if input_number % 2 ==0 :
+    print(f'The entered number {input_number} is Even')
